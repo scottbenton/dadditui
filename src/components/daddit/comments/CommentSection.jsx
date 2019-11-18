24f59385803efacb3ac763ae59d13e4ebffbe5c2
Scott Benton <scott.ma.benton@gmail.com>
2019-11-18 00:37:55 -0500

    Restructured how comments were stored so that we could listen for changes to the comments objects from the realtime database

--- src/components/daddit/comments/CommentSection.jsx
+++ src/components/daddit/comments/CommentSection.jsx
@@ -2,45 +2,63 @@ import React, { useEffect } from "react";
 import { makeStyles } from "@material-ui/core/styles";
 import { useDatabase } from "api/firebase/FirebaseDatabase";
 import Typography from "@material-ui/core/Typography";
-import  { getUserNameFromID } from "utilities/databaseHelper";
+import { getUserNameFromID } from "utilities/databaseHelper";
 import { OpenButton } from "components/shared/OpenButton";
-import { DialogWrapper } from "components/shared/DialogWrapper"
-import { CreateComment } from "components/daddit/comments/CreateComment"
+import { DialogWrapper } from "components/shared/DialogWrapper";
+import { CreateComment } from "components/daddit/comments/CreateComment";
 
 const useStyles = makeStyles(theme => ({
-    comment: props => ({
-        padding: theme.spacing(1),
-        margin: theme.spacing(1),
-        marginLeft: theme.spacing(props.commentDepth),
-        marginRight: 0,
-        backgroundColor: props.commentDepth % 2 === 1 ? "#e6e6e6" : theme.palette.background.paper
-    })
+  comment: props => ({
+    padding: theme.spacing(1),
+    margin: theme.spacing(1),
+    marginLeft: theme.spacing(props.commentDepth),
+    marginRight: 0,
+    backgroundColor:
+      props.commentDepth % 2 === 1 ? "#e6e6e6" : theme.palette.background.paper
+  })
 }));
 
 export function CommentSection(props) {
-    const { comment, commentDepth } = props;
-    const classes = useStyles({commentDepth});
-    const { database } = useDatabase();
-    const addReply = async (newCommentID) =>
-    database
-      .ref("comments/" + comment.id + "/replyKeys")
-      .update({ key:newCommentID });
-    return(<div className={classes.comment}>
-        <Typography>
-            {comment.text}
-        </Typography>
-        <Typography>
-            {"-" + comment.authorDisplayName}
-        </Typography>
-        <div className="ActionsHolder">
+  const { comments, commentID, commentDepth } = props;
+
+  console.log(comments);
+  const currentComment = comments[commentID];
+
+  const classes = useStyles({ commentDepth });
+  const { database } = useDatabase();
+
+  const addReply = async newCommentID => {
+    const ref = await database.ref("comments/" + commentID + "/replyKeys");
+    const newKey = ref.push().getKey();
+    console.log(newKey, newCommentID);
+    ref.update({ ["/" + newKey]: newCommentID });
+  };
+  return (
+    <>
+      {currentComment && (
+        <div className={classes.comment}>
+          <Typography>{currentComment.text}</Typography>
+          <Typography>{"-" + currentComment.authorDisplayName}</Typography>
+          <div className="ActionsHolder">
             <OpenButton buttonContent="reply">
-                <DialogWrapper title="replying">
-                    <CreateComment addCommentToParent={addReply}/>
-                </DialogWrapper>
+              <DialogWrapper title="replying">
+                <CreateComment addCommentToParent={addReply} />
+              </DialogWrapper>
             </OpenButton>
+          </div>
+          {currentComment.replyKeys &&
+            Object.values(
+              currentComment.replyKeys
+            ).map((replyKey, commentIndex) => (
+              <CommentSection
+                key={commentIndex}
+                comments={comments}
+                commentID={replyKey}
+                commentDepth={commentDepth + 1}
+              />
+            ))}
         </div>
-        {Array.isArray(comment.replies) && comment.replies.map( (reply, commentIndex) =>
-            <CommentSection key={commentIndex} comment={reply} commentDepth={commentDepth+1}/>
-        )}
-    </div>);
+      )}
+    </>
+  );
 }
--- src/components/daddit/comments/ViewComments.jsx
+++ src/components/daddit/comments/ViewComments.jsx
@@ -2,8 +2,9 @@ import React, { useEffect } from "react";
 import { makeStyles } from "@material-ui/core/styles";
 import { useDatabase } from "api/firebase/FirebaseDatabase";
 import { Typography } from "@material-ui/core";
-import  { getUserNameFromID } from "utilities/databaseHelper";
+import { getUserNameFromID } from "utilities/databaseHelper";
 import { CommentSection } from "./CommentSection";
+import { updateStateObjectByKey } from "utilities/StateHelpers";
 
 const useStyles = makeStyles(theme => ({}));
 
@@ -11,52 +12,89 @@ export function ViewComments(props) {
   const { parentComment, callback } = props;
   const { database } = useDatabase();
 
+  const { id } = parentComment;
+
+  const [loading, setLoading] = React.useState(false);
   const [comments, setComments] = React.useState();
+  const updateCommentByID = (id, comment) =>
+    updateStateObjectByKey(id, comment, setComments);
 
   useEffect(() => {
-    const snapshotHelper = async dataPromise => {
-      const snapshot = await dataPromise;
-      return snapshot.val();
-    };
+    const snapshotFunction = async snapshot => {
+      console.log(snapshot);
+      const commentID = snapshot.ref.key;
 
-    const loadComments = async currentComment => {
-      return new Promise(async (resolve, reject) => {
-        let newComment = { ...currentComment };
-        const authorPromise = getUserNameFromID(newComment.author, database);
-        let replies = [];
-        if (typeof currentComment.replyKeys === "object") {
-          replies = Object.values(currentComment.replyKeys);
-        }
-        if (replies.length > 0) {
-          const replyPromises = replies.map(async replyKey => {
-            const reply = await snapshotHelper(
-              database.ref("comments/" + replyKey).once("value")
-            );
-            return loadComments(reply);
-          });
-          const fulfilledReplies = await Promise.all(replyPromises);
-          newComment.replies = fulfilledReplies.map((reply, replyIndex) => {
-            return {...reply, id:replies[replyIndex]}
-          });
-          newComment.authorDisplayName = await authorPromise;
-          resolve(newComment);
-        } else {
-          newComment.authorDisplayName = await authorPromise;
-          resolve(newComment);
-        }
-      });
-    };
+      const commentNoAuthor = snapshot.val();
+      console.log(commentNoAuthor);
+      const replyPromises =
+        commentNoAuthor.replyKeys &&
+        Object.values(commentNoAuthor.replyKeys).map(key => loadComments(key));
 
-    if (parentComment && database) {
-      loadComments(parentComment).then(newComments => {
-        setComments(newComments);
-      });
+      const authorPromise = getUserNameFromID(commentNoAuthor.author, database);
+
+      let comment = { ...commentNoAuthor };
+      comment.authorDisplayName = await authorPromise;
+
+      replyPromises && (await Promise.all(replyPromises));
+      updateCommentByID(commentID, comment);
+    };
+    //   const snapshotHelper = async dataPromise => {
+    //     const snapshot = await dataPromise;
+    //     return snapshot.val();
+    //   };
+
+    const loadComments = commentID =>
+      database.ref("comments/" + commentID).on("value", snapshotFunction);
+    //   const loadComments = async currentComment => {
+    //     return new Promise(async (resolve, reject) => {
+    //       let newComment = { ...currentComment };
+    //       const authorPromise = getUserNameFromID(newComment.author, database);
+    //       let replies = [];
+    //       if (typeof currentComment.replyKeys === "object") {
+    //         replies = Object.values(currentComment.replyKeys);
+    //       }
+    //       if (replies.length > 0) {
+    //         const replyPromises = replies.map(async replyKey => {
+    //           const reply = await snapshotHelper(
+    //             database.ref("comments/" + replyKey).once("value")
+    //           );
+    //           return loadComments(reply);
+    //         });
+    //         const fulfilledReplies = await Promise.all(replyPromises);
+    //         newComment.replies = fulfilledReplies.map((reply, replyIndex) => {
+    //           return { ...reply, id: replies[replyIndex] };
+    //         });
+    //         newComment.authorDisplayName = await authorPromise;
+    //         resolve(newComment);
+    //       } else {
+    //         newComment.authorDisplayName = await authorPromise;
+    //         resolve(newComment);
+    //       }
+    //     });
+    //   };
+
+    if (id && database && typeof comments !== "object") {
+      setComments({});
+      loadComments(id);
     }
-  }, [parentComment, database]);
+
+    return () => {
+      typeof comments === "object" &&
+        Object.keys(comments).forEach(commentID => {
+          database.ref("comments/" + commentID).off("value", snapshotFunction);
+        });
+    };
+  }, [id, database, comments]);
 
   useEffect(() => {
     console.log(comments);
   }, [comments]);
 
-  return <>{comments && <CommentSection comment={comments} commentDepth={0}/>}</>;
+  return (
+    <>
+      {typeof comments === "object" && (
+        <CommentSection comments={comments} commentID={id} commentDepth={0} />
+      )}
+    </>
+  );
 }
